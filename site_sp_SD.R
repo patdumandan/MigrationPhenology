72e2b1888347faa0ba2d3af27ed0004ce16c9db2
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2022-09-16 15:30:05 -0400

    decompose diversity index values

--- site_sp_SD.R
+++ site_sp_SD.R
@@ -1,4 +1,4 @@
-#oOshutes sample####
+#Goshutes sample####
 require(dplyr)
 require(lubridate)
 require(tidyr)
@@ -9,10 +9,10 @@ require(ggplot2)
 #GOSHUTES####
 GosMts=read.csv("GosMts.csv")
 
-#gos=GosMts%>%select(-TOTAL)%>%
-#  pivot_longer(cols=3:27, names_to = "Species", values_to="Count")%>%
-#  mutate(year=year(Date), month=month(Date), date=day(Date))%>%
-#  filter(!is.na(Count))
+gos=GosMts%>%select(-TOTAL)%>%
+  pivot_longer(cols=3:27, names_to = "Species", values_to="Count")%>%
+  mutate(year=year(Date), month=month(Date), date=day(Date))%>%
+  filter(!is.na(Count))
 
 GosMts[, 3:27][is.na(GosMts[, 3:27])] <- 0
 
@@ -105,7 +105,7 @@ f1=hsw%>%group_by(YR)%>%filter(sd==max(sd))
 
 #species-level####
 
-hms_sp=hms%>%
+hms_sp=hms%>%select(-UNID.ACCIPITER, -UNID.BUTEO,-UNID.EAGLE, -UNID.FALCON, -UNID.RAPTOR)%>%
   pivot_longer(cols=6:30, names_to = "Species", values_to="Count")%>%
   filter(!is.na(Count), !YR<1983, !Species%in%c("UNID.ACCIPITER",
                                                 "UNID.BUTEO", 
@@ -136,7 +136,7 @@ ggplot(indtab2, aes(x=Julian, y=sptot, col=Species))+geom_label(label=indtab2$Sp
   annotate("rect", xmin=330, xmax=350, alpha=0.1, fill="blue", 
            ymin=0, ymax=80)
 
-#Hawk Ridge###
+#Hawk Ridge####
 HawRd=read.csv("HawkRid.csv")
 HawRd[, 3:24][is.na(HawRd[, 3:24])] <- 0
 
@@ -181,6 +181,115 @@ ggplot(indtab3, aes(x=Julian, y=sptot, col=Species))+geom_label(label=indtab3$Sp
   annotate("rect", xmin=255, xmax=265, alpha=0.1, fill="blue", 
            ymin=0, ymax=350)
 
+####DECOMPOSING DIVERSITY####
+
+#GOSHUTES####
+gos_sp_max=gos%>%mutate(Julian=as.POSIXlt(Date)$yday)%>%
+  group_by(year, Species)%>%
+  filter(Count==max(Count), !Species%in%c("UA", "UB", "UE", "UF", "UR"))%>%slice_head(n=1)%>%
+  arrange(year)
+
+gos_sp=gos%>%mutate(Julian=as.POSIXlt(Date)$yday)%>%
+  group_by(year, Species)%>%
+  summarise(yr_tot=sum(Count))%>%filter(!Species%in%c("UA", "UB", "UE", "UF", "UR"))%>%
+  arrange(year)
+
+ggplot(gos_sp_max, aes(x=year, y=Julian))+geom_point(col="blue")+
+  facet_wrap(~Species)+geom_hline(yintercept=268, linetype=2)
+
+ggplot(gos_sp, aes(x=year, y=log(yr_tot)))+geom_point()+facet_wrap(~Species)+
+  stat_smooth(method="lm")
+
+ggplot(gos_sp_max, aes(x=year, y=Julian))+geom_point()+facet_wrap(~Species)+
+  stat_smooth(method="lm")+geom_hline(yintercept=268, linetype=2)
 
 
+#DAILY TOTALS
+gostot=GosMts%>%select(-TOTAL)%>%
+  mutate(yr_tot=rowSums(.[3:27], na.rm=T), year=year(Date), month=month(Date), date=day(Date))%>%
+  filter(!year>2018)
+gostot$Julian=as.POSIXlt(gostot$Date)$yday 
+
+gostotmax=gostot%>%group_by(year)%>%filter(yr_tot==max(yr_tot))
+
+ggplot(gostotmax, aes(x=year, y=Julian))+geom_point()+geom_hline(yintercept=268, linetype=2)+ggtitle("Goshutes")+
+  stat_smooth(method="lm")  
+
+#hAWK mT####
+#Hawk Mt####
+
+hms=read.csv("HMSDay.csv")
+#combine baea and goea data
+hmstot=hms%>%
+  mutate(BAEA=rowSums(.[9:11]), GOEA=rowSums(.[21:23]))%>%
+  select(-OBSERVERS, -GOEA.I, -GOEA.A, -GOEA.U, -BAEA.I, -BAEA.U, -BAEA.A, -RAPTORS)%>%
+  mutate(yr_tot=rowSums(.[6:30], na.rm=T), date=make_date(YR, MO, DAY))%>%filter(!YR<1983, !YR>2018)
+
+hmstot$Julian=as.POSIXlt(hmstot$date)$yday 
+
+#species-level####
+
+hms_sp=hmstot%>%select(-yr_tot)%>%
+  pivot_longer(cols=6:30, names_to = "Species", values_to="Count")%>%
+  filter(!is.na(Count), !YR<1983, !Species%in%c("UNID.ACCIPITER",
+                                                "UNID.BUTEO", 
+                                                "UNID.EAGLE",
+                                                "UNID.FALCON",
+                                                "UNID.RAPTOR"))%>%
+  mutate(Date=make_date(YR, MO, DAY))
+
+#PEAK DAYS PER SPECIES
+hms_sp_max=hms_sp%>%mutate(Julian=as.POSIXlt(Date)$yday)%>%
+  group_by(YR, Species)%>%
+  filter(Count==max(Count))%>%slice_head(n=1)%>%
+  arrange(YR)
+
+#PEAK DAYS FOR SITE
+hmsmax=hmstot%>%
+  select(-UNID.ACCIPITER, -UNID.BUTEO, -UNID.EAGLE, -UNID.FALCON, -UNID.RAPTOR)%>%
+  mutate(Julian=as.POSIXlt(date)$yday)%>%
+  group_by(YR)%>%
+  filter(yr_tot==max(yr_tot))%>%
+  arrange(YR)
+
+ggplot(hmsmax, aes(x=YR, y=Julian))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  geom_hline(yintercept=260, linetype=2)+ylab("peak migration date(Julian)")
+
+ggplot(hms_sp_max, aes(x=YR, y=Julian))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  facet_wrap(~Species)+geom_hline(yintercept=260, linetype=2)+ylab("peak migration date(Julian)")
+
+ggplot(hms_sp_max, aes(x=YR, y=Count))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  facet_wrap(~Species)+geom_hline(yintercept=260, linetype=2)+ylab("max counts (log)")
+
+#Hawk Ridge####
+HawRd=read.csv("HawkRid.csv")
+HawRd[, 3:24][is.na(HawRd[, 3:24])] <- 0
+
+#without unknowns
+hrtot=HawRd%>%select(-TOTAL)%>%
+  mutate(yr_tot=rowSums(.[3:19], na.rm=T), year=year(Date), month=month(Date), date=day(Date))%>%
+  filter(!year>2018) #get annual totals across all spp.
+
+hrtot$Julian=as.POSIXlt(hrtot$Date)$yday 
+
+#species-level####
+
+hr_sp=rsw%>%
+  pivot_longer(cols=2:18, names_to = "Species", values_to="Count")%>%
+  filter(!is.na(Count))
+
+hr_sp_max=hr_sp%>%group_by(Species,year)%>%filter(Count==max(Count))%>%slice_head(n=1)%>%select(-yr_tot)
+hrmax=hrtot%>%group_by(year)%>%filter(yr_tot==max(yr_tot))%>%slice_head(n=1)%>%select(-UA, -UB,-UE,-UF,-UR)%>%
+  mutate(Julian=as.POSIXlt(Date)$yday )
+
+#plot
+
+ggplot(hrmax, aes(x=year, y=Julian))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  geom_hline(yintercept=257, linetype=2)+ylab("peak migration date(Julian)")
+
+ggplot(hr_sp_max, aes(x=year, y=Julian))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  facet_wrap(~Species)+geom_hline(yintercept=257, linetype=2)+ylab("peak migration date(Julian)")
+
+ggplot(hr_sp_max, aes(x=year, y=Count))+geom_point()+stat_smooth(method="lm")+theme_classic()+
+  facet_wrap(~Species)+geom_hline(yintercept=257, linetype=2)+ylab("max counts (log)")
 
