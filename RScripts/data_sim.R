891f19cbbd91cf10720a2dc0dabfd516644f0490
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2024-01-02 12:37:36 -0500

    add simulation results for relative-abundance weighted metric [minor]

--- RScripts/data_sim.R
+++ RScripts/data_sim.R
@@ -1,15 +1,29 @@
+# toy model for calculating the effect of species' phenology and abundance on aggregate measures
+
+#scenario: community-level delayed migration
+
 set.seed(1234)
 
 nrep=20
 nsp=3
 
 #create species data frame
+
+#data simulation####
+
+#scenario 1####
+# assume that sp 1 is commonest, is a late migrant, and is delaying migration
+# assume that sp 2 and 3 are less common and are not changing migration timing
+
+
 sp_dat=data.frame(
   year=rep(1980:2000, each=nsp, length.out=nrep*nsp),
   species=as.character(rep(1:3, length.out=nrep*nsp)))
 
 sp_dat1=sp_dat%>%
   mutate(
+    rel_abund=case_when(species=="1" ~ 0.8,
+                        species%in%c("2", "3") ~ 0.1),
     period=case_when(year%in%c(1980:1984) ~"T1",
                      year%in%c(1995:1999) ~ "T2",
                      year%in%c(1985:1994)~ "TN"),
@@ -20,16 +34,14 @@ sp_dat1=sp_dat%>%
                      species%in%c("1", "2", "3") & period=="TN"~ rpois(year%in%c(1985:1994), 202)))
 
 #summarize unit-level values
-sp_sum1=sp_dat1%>%group_by(species, period)%>%summarise(u_pd=mean(pd_10))%>%filter(!period=="TN")
+sp_sum1=sp_dat1%>%group_by(species, period, rel_abund)%>%
+  summarise(u_pd=mean(pd_10))%>%filter(!period=="TN")
 
 s1=sp_dat1%>%filter(year%in%c(1980:1984))
 s2=sp_dat1%>%filter(year%in%c(1995:1999))
 
 mean(s1$pd_10)
 mean(s2$pd_10)
-
-#calculate phenological shift 
-phen_sp=mean(s2$pd_10)-mean(s1$pd_10)
 
 #create community data
 
@@ -44,7 +56,7 @@ mean(c2$pd_10)
 #calculate phenological shift
 phen_comm=mean(c2$pd_10)-mean(c1$pd_10)
 
-#plot
+#visualize data####
 
 ggplot(comm_dat, aes(x=year, y=pd_10))+geom_line()+
   geom_hline(yintercept=mean(comm_dat$pd_10), lty=2)+
@@ -62,4 +74,33 @@ ggplot(sp_dat1, aes(x=year, y=pd_10))+geom_line()+
   annotate("rect",xmin=1999, xmax=1995, ymin=min(sp_dat$pd_10), ymax=max(sp_dat$pd_10), 
            color="red", alpha=0.1, fill="red")+theme_classic()+
   ggtitle("unit-level")+ylab("10% passage date")
+
+# metrics for phenological shift#### 
+
+##mean species-level shift#### 
+
+#calculate phenological shift (species level)
+phen_sp=mean(s2$pd_10)-mean(s1$pd_10)
+
+#calculate phenological shift (community level)
+phen_comm=mean(c2$pd_10)-mean(c1$pd_10)
+
+##relative abundance-weighted phenological shift####
+
+sp11=sp_sum1%>%pivot_wider(names_from = period, values_from=u_pd)%>%
+  mutate(phen_shift=T2-T1,
+         wgt_shift=rel_abund*phen_shift)
+
+#contribution to community-level shift####
+
+phen_comm #community phenology shift (~3 days delay)
+
+#across all species contribution
+
+phen_sp_cont=sum(sp11$wgt_shift)/phen_comm
+
+#per species contribution
+
+phen_sp_cont_per_sp=sp11%>%
+  mutate(cont_sp=wgt_shift/sum(wgt_shift))
 
